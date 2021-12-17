c535e976d97f2c5dce9edf3b62625902759a61d5
tmmlaarhoven <mail@thijs.com>
2021-12-17 22:57:57 +0100

    Auto-updater - 2021-12-17-22-57-56

--- rankings/autoupdate.sh
+++ rankings/autoupdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-updtime=$(date)
+date_stamp=$(date +"%F-%H-%M-%S")
 git pull
-# rem ScanPlayers.py
-LigaUpdater.py
-FetchData.py
-Caller.py
-DensityPlot.py
+# python3 ScanPlayers.py
+#python3 LigaUpdater.py
+#python3 FetchData.py
+#python3 Caller.py
+#python3 DensityPlot.py
 git add --all --verbose
-git commit -m "Auto-updater - %updtime%" --verbose
+git commit -m "Auto-updater - $date_stamp" --verbose
 git push --verbose
