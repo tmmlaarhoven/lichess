98659d5d15dddd61c762336fd69b4686a466a956
tmmlaarhoven <mail@thijs.com>
2022-10-04 13:02:19 +0200

    Auto-updater - 2022-10-04 11:27:28

---
+++ rankings/Caller-AllAll.py
@@ -0,0 +1,32 @@
+import requests
+import re
+import os
+import time
+import collections
+import os.path
+import ndjson
+import shutil
+from Utilities import *
+
+print("\n=== Starting Caller.py ===\n")
+
+# Default: No plots
+DrawPlots = False
+
+BuildIndexPage()
+
+Cat = dict()
+for V in {"all"}:
+	Cat[V] = dict()
+	for E in {"all"}:
+
+		# Always first update everything else
+		Cat[V][E] = ArenaCategory(V, E)
+		Cat[V][E].LoadRankings()
+		Cat[V][E].UpdateRankings()
+		if DrawPlots or E in {"marathon", "titled"}:
+			Cat[V][E].UpdatePlots()
+		Cat[V][E].UpdateWebsite()
+		del Cat[V][E]
+
+print("\n=== Finished Caller.py ===\n")
