832f2c80aa9c9eca300058b842483b0a5be99d72
tmmlaarhoven <mail@thijs.com>
2024-03-29 02:03:52 +0100

    Auto-updater - 2024-03-28 23:47:53

---
+++ rankings/CallerPlotsOnly.py
@@ -0,0 +1,33 @@
+import requests
+import re
+import os
+import time
+import collections
+import os.path
+import ndjson
+import shutil
+from Utilities import *
+
+print("\n=== Starting Caller.py ===\n")
+
+# Default: No plots
+DrawPlots = True
+
+BuildIndexPage()
+BuildPlayerIndex()
+
+Cat = dict()
+for V in AllVariants:
+	Cat[V] = dict()
+	for E in AllEvents:
+
+		# Always first update everything else
+		Cat[V][E] = ArenaCategory(V, E)
+		Cat[V][E].LoadRankings()
+		Cat[V][E].UpdateRankings()
+		if DrawPlots:
+			Cat[V][E].UpdatePlots()
+		Cat[V][E].UpdateWebsite()
+		del Cat[V][E]
+
+print("\n=== Finished Caller.py ===\n")
