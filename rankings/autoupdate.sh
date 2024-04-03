6195856eb12e8da328321fdfe0530452d91c4b32
tmmlaarhoven <mail@thijs.com>
2024-04-03 18:01:38 +0200

    Auto-updater - 2024-04-03 15:55:34

--- rankings/autoupdate.sh
+++ rankings/autoupdate.sh
@@ -4,8 +4,17 @@ git pull
 # python3 ScanPlayers.py
 # python3 LigaUpdater.py
 python3 FetchData.py
-python3 Caller.py
-python3 DensityPlot.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+python3 FetchData.py
+# python3 Caller.py
+# python3 DensityPlot.py
 git add --all --verbose
 git commit -m "Auto-updater - $date_stamp" --verbose
 git push --verbose
