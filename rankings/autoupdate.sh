8565179861072723ec1841077480463e81ce8d19
tmmlaarhoven <mail@thijs.com>
2024-04-04 02:34:24 +0200

    Auto-updater - 2024-04-04 01:30:01

--- rankings/autoupdate.sh
+++ rankings/autoupdate.sh
@@ -2,19 +2,10 @@
 date_stamp=$(date +"%F %H:%M:%S")
 git pull
 # python3 ScanPlayers.py
-# python3 LigaUpdater.py
+python3 LigaUpdater.py
 python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-python3 FetchData.py
-# python3 Caller.py
-# python3 DensityPlot.py
+python3 Caller.py
+python3 DensityPlot.py
 git add --all --verbose
 git commit -m "Auto-updater - $date_stamp" --verbose
 git push --verbose
